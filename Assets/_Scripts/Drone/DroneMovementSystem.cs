9f377faec4c12e40b98ed3eaddac16f254ea13de
lentar <lntr.exe@gmail.com>
2024-09-17 17:54:41 +0300

    Joystick Movement Added

--- Assets/_Scripts/Drone/DroneMovementSystem.cs
+++ Assets/_Scripts/Drone/DroneMovementSystem.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 
 public class DroneMovementSystem : MonoBehaviour
 {
+    [Header("Dependencies")]
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private DronePropertiesHolderSO _dronePropertiesHolderSO;
+
+    [Header("Motors")]
     [SerializeField] private float _throttleMultiplicator;
-    [SerializeField] private float _accelerationMultiplier;
+    [SerializeField] private float _motorsAccelerationMultiplier;
+    [SerializeField] private float _motorsPowerClamp;
 
     private float _droneSpeed;
     private float _yawMotorPower;
     private float _throttleMotorPower;
+    private float _compensateFixedDeltaTimeMultiplier = 200f;
     private Vector2 _pitchAndRollMotorPower;
     private DroneMovementInputActionsReader _droneMovementInputActionsReader;
 
@@ -41,24 +46,16 @@ public class DroneMovementSystem : MonoBehaviour
 
     private void HandlePitchAndRoll()
     {
-        Vector2 pitchAndRollDelta = _droneMovementInputActionsReader.GetPitchAndRollInputValue;
-        if (pitchAndRollDelta == Vector2.zero)
+        Vector2 inputVector = _droneMovementInputActionsReader.GetPitchAndRollInputValue;
+        if (inputVector == Vector2.zero)
         {
-            if (_pitchAndRollMotorPower == Vector2.zero)
-            {
+            Vector2 minPower = Vector2.one * 0.01f;
+            if (Mathf.Abs(_pitchAndRollMotorPower.x) < minPower.x && Mathf.Abs(_pitchAndRollMotorPower.y) < minPower.y)
                 return;
-            }
-            else
-            {
-                SmootlyPowerDownPitchAndRoll();
-            }
         }
-        else
-        {
-            _pitchAndRollMotorPower += pitchAndRollDelta * _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            _pitchAndRollMotorPower.x = Mathf.Sign(_pitchAndRollMotorPower.x) * Mathf.Clamp01(Mathf.Abs(_pitchAndRollMotorPower.x));
-            _pitchAndRollMotorPower.y = Mathf.Sign(_pitchAndRollMotorPower.y) * Mathf.Clamp01(Mathf.Abs(_pitchAndRollMotorPower.y));
-        }
+
+        _pitchAndRollMotorPower.x = GetAdjustedMotorPowerAccordingInputValue(inputVector.x, _pitchAndRollMotorPower.x);
+        _pitchAndRollMotorPower.y = GetAdjustedMotorPowerAccordingInputValue(inputVector.y, _pitchAndRollMotorPower.y);
 
         if (_pitchAndRollMotorPower != Vector2.zero)
         {
@@ -66,115 +63,45 @@ public class DroneMovementSystem : MonoBehaviour
         }
     }
 
-    private void SmootlyPowerDownPitchAndRoll()
-    {
-        if (_pitchAndRollMotorPower.y > 0f)
-        {
-            _pitchAndRollMotorPower.y -= _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            if (_pitchAndRollMotorPower.y < 0f)
-                _pitchAndRollMotorPower.y = 0f;
-        }
-        else
-        {
-            _pitchAndRollMotorPower.y += _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            if (_pitchAndRollMotorPower.y > 0f)
-                _pitchAndRollMotorPower.y = 0f;
-        }
-        if (_pitchAndRollMotorPower.x > 0f)
-        {
-            _pitchAndRollMotorPower.x -= _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            if (_pitchAndRollMotorPower.x < 0f)
-                _pitchAndRollMotorPower.x = 0f;
-        }
-        else
-        {
-            _pitchAndRollMotorPower.x += _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            if (_pitchAndRollMotorPower.x > 0f)
-                _pitchAndRollMotorPower.x = 0f;
-        }
-    }
-
     private void HandleYaw()
     {
-        float yawDelta = _droneMovementInputActionsReader.GetYawInputValue;
-        if (yawDelta == 0f)
+        float inputValue = _droneMovementInputActionsReader.GetYawInputValue;
+        if (inputValue == 0f)
         {
-            if (_yawMotorPower == 0f)
-            {
+            if (Mathf.Abs(_yawMotorPower) < 0.01f)
                 return;
-            }
-            else
-            {
-                SmootlyPowerDownYaw();
-            }
-        }
-        else
-        {
-            _yawMotorPower += yawDelta * _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            _yawMotorPower = Mathf.Sign(_yawMotorPower) * Mathf.Clamp01(Mathf.Abs(_yawMotorPower));
         }
 
+        _yawMotorPower = GetAdjustedMotorPowerAccordingInputValue(inputValue, _yawMotorPower);
         if (_yawMotorPower != 0f)
         {
             transform.Rotate(new Vector3(0f, _yawMotorPower * _droneSpeed, 0f), Space.Self);
         }
     }
 
-    private void SmootlyPowerDownYaw()
-    {
-        if (_yawMotorPower > 0f)
-        {
-            _yawMotorPower -= _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            if (_yawMotorPower < 0f)
-                _yawMotorPower = 0f;
-        }
-        else
-        {
-            _yawMotorPower += _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            if (_yawMotorPower > 0f)
-                _yawMotorPower = 0f;
-        }
-    }
-
     private void HandleThrottle()
     {
-        float throttleDelta = _droneMovementInputActionsReader.GetThrottleInputValue;
-        if (throttleDelta == 0f)
-        {
-            if (_throttleMotorPower == 0f)
-            {
+        float inputValue = Mathf.Clamp01(_droneMovementInputActionsReader.GetThrottleInputValue);
+        if (inputValue == 0f)
+        { 
+            if(_throttleMotorPower < 0.01f)
                 return;
-            }
-            else
-            {
-                SmootlyPowerDownThrottle();
-            }
-        }
-        else
-        {
-            _throttleMotorPower += throttleDelta * _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            _throttleMotorPower = Mathf.Sign(_throttleMotorPower) * Mathf.Clamp01(Mathf.Abs(_throttleMotorPower));
         }
 
+        _throttleMotorPower = GetAdjustedMotorPowerAccordingInputValue(inputValue, _throttleMotorPower);
         if (_throttleMotorPower != 0f)
         {
             _rigidbody.AddForce(transform.up * _throttleMultiplicator * _throttleMotorPower * _droneSpeed);
         }
+
+        Debug.Log(inputValue + "   " + _throttleMotorPower);
     }
 
-    private void SmootlyPowerDownThrottle()
+    private float GetAdjustedMotorPowerAccordingInputValue(float inputValue, float currentMotorPowerValue)
     {
-        if (_throttleMotorPower > 0f)
-        {
-            _throttleMotorPower -= _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            if (_throttleMotorPower < 0f)
-                _throttleMotorPower = 0f;
-        }
-        else
-        {
-            _throttleMotorPower += _droneSpeed * _accelerationMultiplier * Time.fixedDeltaTime;
-            if (_throttleMotorPower > 0f)
-                _throttleMotorPower = 0f;
-        }
+        float normalizedMotorPower = currentMotorPowerValue / _motorsPowerClamp;
+        float t = _motorsAccelerationMultiplier * Time.fixedDeltaTime;
+        normalizedMotorPower = Mathf.Lerp(normalizedMotorPower, inputValue, t);
+        return normalizedMotorPower * _motorsPowerClamp;
     }
 }
