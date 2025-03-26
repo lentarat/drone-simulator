61cbc07cd2a49c474f732bb1974969c5decf4724
lentar <lntr.exe@gmail.com>
2025-03-26 23:30:51 +0200

    Windows update

--- Assets/_Scripts/UI/Windows/ConcreteWindows/GameModeWindow.cs
+++ Assets/_Scripts/UI/Windows/ConcreteWindows/GameModeWindow.cs
@@ -1,4 +1,27 @@
-public class GameModeWindow : BaseWindow
-{
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+public class GameModeWindow : BackableWindow
+{
+    [Header("GameModeWindow field")]
+    [SerializeField] private Button _playButton;
+
+    protected override WindowState[] GetChosenWindowStates()
+    {
+        WindowState[] windowStates = { new FadedWindowState(this) };
+        return windowStates;
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        SubscribeToButton();
+    }
+
+    private void SubscribeToButton()
+    {
+        _playButton.onClick.AddListener(() => { SceneManager.LoadScene(1); });
+    }
 }
