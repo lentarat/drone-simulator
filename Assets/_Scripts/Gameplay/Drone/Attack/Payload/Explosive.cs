0aa5b4e4d899b59dc176507f75ed0279881ae561
lentar <lntr.exe@gmail.com>
2025-04-22 11:49:49 +0300

    Target dying from explosives

--- Assets/_Scripts/Gameplay/Drone/Attack/Payload/Explosive.cs
+++ Assets/_Scripts/Gameplay/Drone/Attack/Payload/Explosive.cs
@@ -5,7 +5,10 @@ using Zenject.SpaceFighter;
 
 public class Explosive : DronePayload
 {
+    //[SerializeField] private float _damage;
+    [SerializeField] private float _maxDistanceDealingDamage;
     [SerializeField] private ParticleSystem _explosionVFX;
+    [SerializeField] private LayerMask _damageableLayerMask;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -14,7 +17,28 @@ public class Explosive : DronePayload
 
     private void Explode()
     {
+        FindDamageables();
         ParticleSystem explosionVFX = Instantiate(_explosionVFX, transform.position, _explosionVFX.transform.rotation);
         Destroy(gameObject);
+    }
+
+    private Collider[] FindDamageables()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _maxDistanceDealingDamage, _damageableLayerMask);
+        if (colliders != null)
+        {
+            Debug.Log(colliders.Length);
+            foreach (Collider collider in colliders)
+            {
+                collider.GetComponent<IDamageable>()?.ApplyDamage();
+            }
+        }
+        return colliders;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, _maxDistanceDealingDamage);
     }
 }
