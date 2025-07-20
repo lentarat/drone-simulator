d0d96c35dc8372b1570afc3b9f5db583d2b6d815
lentar <lntr.exe@gmail.com>
2025-07-21 01:03:47 +0300

    Application quit ability added

--- Assets/_Scripts/UI/UI/Windows/ConcreteWindows/MainMenu/MainMenuWindow.cs
+++ Assets/_Scripts/UI/UI/Windows/ConcreteWindows/MainMenu/MainMenuWindow.cs
@@ -1,4 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
 public class MainMenuWindow : BaseWindow
 {
+    [SerializeField] private Button _quitButton;
 
+    protected override void Awake()
+    {
+        base.Awake();
+        SubscribeToQuitButton();
+    }
+
+    private void SubscribeToQuitButton()
+    {
+        _quitButton.onClick.AddListener(QuitApplication);
+    }
+
+    private void QuitApplication()
+    { 
+        Application.Quit();
+    }
 }
